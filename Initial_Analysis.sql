de218bb9d3930ebf91d50bb5b16ef54310a2b562
cannem@uiowa.edu <cannem@uiowa.edu>
2022-09-25 04:37:28 +0000

    changes

--- Initial_Analysis.sql
+++ Initial_Analysis.sql
@@ -1,2 +1,18 @@
 -- Databricks notebook source
+use group3
+
+-- COMMAND ----------
+
+describe iowa_liquor_sales
+
+-- COMMAND ----------
+
+select count(distinct store_no) from iowa_liquor_sales
+
+-- COMMAND ----------
+
+select sum(sale_dollars) as sales,month_of_sale,year_of_sale from iowa_liquor_sales group by month_of_sale,year_of_sale order by month_of_sale,year_of_sale
+
+-- COMMAND ----------
+
 
